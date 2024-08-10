8b1c26105e1e172e8b19d1e25a80dd3d9ba85b61
WesitosFsa <137083225+WesitosFsa@users.noreply.github.com>
2024-08-09 21:44:17 -0500

    Buenas

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import './App.css'
+import { PaginaInicial } from './pages/PaginaInicial.jsx'
+import { Productos } from './pages/Productos.jsx'
+import Ingresar from './pages/Ingresar.jsx'
+import { Registrar } from './pages/Registrar.jsx'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { AuthProvider } from '../context/AuthProvider'
+import Auth from './layout/Auth'
+import { PrivateRoute } from './routes/PrivateRoute.jsx'
+import {Forgot} from './pages/Forgot.jsx'
+import { NoEncontrada } from './pages/NoEncontrada.jsx';
+import { Confirmar } from './pages/ConfirmarEmail';
+import Restablecer from './pages/Restablecer';
+import Listar from './pages/Listar.jsx'
+import Crear from './pages/Crear.jsx'
+import BuscarProducto from './pages/BuscarProducto.jsx'
+import Dashboard from './layout/Dashboard.jsx'
+import Confirmacion_registro_tienda from './pages/Confirmacion_registro_tienda.jsx'
+function App() {
+  return (
+
+    <BrowserRouter>
+    <AuthProvider>
+      <Routes>
+        <Route index element={<PaginaInicial/>}/>
+            <Route path='/' element={<Auth/>}>
+            <Route path='ingresar' element={<Ingresar/>}/>
+            <Route path='registrar' element={<Registrar/>}/>
+            <Route path='usuario/confirmar/:token' element={<Confirmar/>}/>
+            <Route path='forgot/:id' element={<Forgot/>}/>
+            <Route path='recuperar-password/:token' element={<Restablecer />} />
+            <Route path='dashboard' element={<Dashboard />} />
+            <Route path='dashboard/productos' element={<Productos/>}/>
+            <Route path='dashboard/listar' element={<Listar />} />
+            <Route path='dashboard/confirmacion' element={<Confirmacion_registro_tienda />} />
+            <Route path='dashboard/crear' element={<Crear/>} />
+           
+            <Route path='dashboard/buscar' element={<BuscarProducto/>} />
+            <Route path='*' element={<NoEncontrada />} />
+        </Route>
+        </Routes>
+    </AuthProvider>
+    </BrowserRouter>
+  )
+}
+
+export default App
